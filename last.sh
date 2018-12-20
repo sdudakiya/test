366c3bf51144a393c47c00cbcbf275ab551e8d93
Sourabh <sdudakiya@gmail.com>
2018-12-21 01:27:27 +0530

    cleanup

--- last.sh
+++ last.sh
@@ -1,8 +1,6 @@
 export GIT_COMMITTER_DATE="Mon 20 Aug 2018 20:19:19 BST" git commit --amend --no-edit --date "Mon 20 Aug 2018 20:19:19 BST"
 echo "test" > commit.md
-export GIT_AUTHOR_DATE="2018-12-20 04:00:00"
 git add commit.md -f
-git commit --date="2018-12-20 00:00:00" -m "test"
 git push origin master
 git rm -rf 20**
 git commit -am "cleanup"
